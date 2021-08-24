8e537e900d7af8215173e7d3fd86b276c1a7a10c
MinhVX <MinhVX@DESKTOP-J45AC56>
2021-08-24 08:38:26 +0700

    Add project files.

---
+++ dangnhap/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dangnhap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
